35c1984b3c8f06ec183f95448bbe59f6792f19d8
Rasmus Kaj <kaj@kth.se>
2015-01-20 15:16:01 +0100

    Move GetUser to user.go.

--- src/parkour/server.go
+++ src/parkour/server.go
@@ -14,8 +14,6 @@ import (
 	"math/rand"
 	"net/http"
 	"net/url"
-	"os/exec"
-	"regexp"
 	"strings"
 	"time"
 )
@@ -182,42 +180,6 @@ func getBout(id *bson.ObjectId) *Bout {
 		return nil
 	} else {
 		panic(err)
-	}
-}
-
-func GetUser(kthid string) User {
-	mgo_conn := mgo_session.Copy()
-	defer mgo_conn.Close()
-
-	var result User
-	err := mgo_conn.DB(DB_name).C("users").Find(bson.M{"kthid": kthid}).One(&result)
-	if err == nil {
-		return result
-	} else {
-		// No such user!  Check with LDAP!
-        // return User{kthid, kthid, kthid}
-		out, err := exec.Command("ldapsearch", "-x", "-LLL", "ugKthid="+kthid).Output()
-		if err != nil {
-			panic(err)
-		}
-		reg := regexp.MustCompile("(?m)^([^:]+): ([^\n]+)$")
-		matches := reg.FindAllStringSubmatch(string(out), -1)
-		matchmap := make(map[string]string)
-		for _, match := range matches {
-			key := match[1]
-			value := match[2]
-			matchmap[key] = value
-		}
-		// username := matchmap["uid"]
-		result.Kthid = kthid
-		result.Name = matchmap["cn"]
-		result.Firstname = matchmap["givenName"]
-
-		err = mgo_conn.DB(DB_name).C("users").Insert(result)
-		if err != nil {
-			panic(err)
-		}
-		return result
 	}
 }
 
--- src/parkour/user.go
+++ src/parkour/user.go
@@ -1,8 +1,49 @@
 package parkour
 
+import (
+    "gopkg.in/mgo.v2/bson"
+    "os/exec"
+    "regexp"
+)
+
 type User struct {
-	Kthid     string
-	Firstname string
-	Name      string
+    Kthid     string
+    Firstname string
+    Name      string
 }
 
+func GetUser(kthid string) User {
+    mgo_conn := mgo_session.Copy()
+    defer mgo_conn.Close()
+
+    var result User
+    err := mgo_conn.DB(DB_name).C("users").Find(bson.M{"kthid": kthid}).One(&result)
+    if err == nil {
+        return result
+    } else {
+        // No such user!  Check with LDAP!
+        // return User{kthid, kthid, kthid}
+        out, err := exec.Command("ldapsearch", "-x", "-LLL", "ugKthid="+kthid).Output()
+        if err != nil {
+            panic(err)
+        }
+        reg := regexp.MustCompile("(?m)^([^:]+): ([^\n]+)$")
+        matches := reg.FindAllStringSubmatch(string(out), -1)
+        matchmap := make(map[string]string)
+        for _, match := range matches {
+            key := match[1]
+            value := match[2]
+            matchmap[key] = value
+        }
+        // username := matchmap["uid"]
+        result.Kthid = kthid
+        result.Name = matchmap["cn"]
+        result.Firstname = matchmap["givenName"]
+
+        err = mgo_conn.DB(DB_name).C("users").Insert(result)
+        if err != nil {
+            panic(err)
+        }
+        return result
+    }
+}
